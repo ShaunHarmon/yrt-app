3d9395f6c6cb9c2e4ab59a01d44d5768eaeca700
ShaunHarmon <shaun.harmon@gmail.com>
2018-10-31 12:02:20 -0500

    udpate

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/yrt-realtime'));
+app.use(express.static(__dirname + '/dist/yrt-app'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/yrt-realtime/index.html'));
+res.sendFile(path.join(__dirname+'/dist/yrt-app/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
