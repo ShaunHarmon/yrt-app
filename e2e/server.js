98b649531d117d6fe71ed23de19068b1c28bdcad
ShaunHarmon <shaun.harmon@gmail.com>
2018-10-31 11:10:40 -0500

    update

--- e2e/server.js
+++ e2e/server.js
@@ -1,18 +1,17 @@
+
 //Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
-
-
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/yrt-app'));
+app.use(express.static(__dirname + '/dist/<name-of-app>'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname + '/dist/yrt-app/index.html'));
+res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
 });
 
-
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
